0d610f8696e953e3b44d0c6430ad4b812dd6982b
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-06-17 18:41:19 +0200

    Add Promise support

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const universalify = require('universalify')
 const assert = require('assert')
+
 const microtime = require('./microtime')
 
 module.exports = class Limiter {
@@ -11,36 +13,33 @@ module.exports = class Limiter {
     assert(this.db, '.db required')
     this.max = max
     this.duration = duration
-    this.key = 'limit:' + this.id
+    this.key = `limit:${this.id}`
+
+    this.get = universalify.fromCallback(cb => {
+      const { db, duration, key, max } = this
+      const now = microtime.now()
+      const start = now - duration * 1000
+
+      db.multi()
+        .zrange([key, 0, start, 'WITHSCORES'])
+        .zcard([key])
+        .zadd([key, now, now])
+        .zrange([key, 0, 0])
+        .pexpire([key, duration])
+        .exec(function (err, res) {
+          if (err) return cb(err)
+          const count = parseInt(Array.isArray(res[0]) ? res[1][1] : res[1])
+          const oldest = parseInt(Array.isArray(res[0]) ? res[3][1] : res[3])
+          return cb(null, {
+            remaining: count < max ? max - count : 0,
+            reset: Math.floor((oldest + duration * 1000) / 1000000),
+            total: max
+          })
+        })
+    })
   }
 
   inspect () {
     return `<Limiter id=${this.id}, duration=${this.duration}, max=${this.max}>`
-  }
-
-  get (fn) {
-    const db = this.db
-    const duration = this.duration
-    const key = this.key
-    const max = this.max
-    const now = microtime.now()
-    const start = now - duration * 1000
-
-    db.multi()
-      .zrange([key, 0, start, 'WITHSCORES'])
-      .zcard([key])
-      .zadd([key, now, now])
-      .zrange([key, 0, 0])
-      .pexpire([key, duration])
-      .exec(function (err, res) {
-        if (err) return fn(err)
-        const count = parseInt(Array.isArray(res[0]) ? res[1][1] : res[1])
-        const oldest = parseInt(Array.isArray(res[0]) ? res[3][1] : res[3])
-        return fn(null, {
-          remaining: count < max ? max - count : 0,
-          reset: Math.floor((oldest + duration * 1000) / 1000000),
-          total: max
-        })
-      })
   }
 }
